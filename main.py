b0a46898902d69547e07978b96a2a94b1d61aafd
Luis Hernandez <luis.hernandez1@ucalgary.ca>
2026-01-20 01:49:04 -0700

    turned them into lists

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import json
 
 # Get the data from sensor 1
 try:
-    with open("SensorData1.csv", "r") as h:
-        sensorData1 = csv.reader(h)
+    sensorLongitude1, sensorLatitude1 = [], []
+    with open("SensorData1.csv", newline="") as h:
+        sensorData1 = csv.DictReader(h)
+
+        for row in sensorData1:
+            sensorLatitude1.append(row["latitude"])
+            sensorLongitude1.append(row["longitude"])
+
 except FileNotFoundError:
     print("CSV file not found")
 except PermissionError:
@@ -13,9 +19,17 @@ except PermissionError:
 
 # Get the data from sensor 2
 try: 
+    sensorLongitude2, sensorLatitude2 = [], []
     with open("SensorData2.json", "r") as f:
         sensorData2 = json.load(f)
+
+    for row in sensorData2:
+            sensorLatitude2.append(row["latitude"])
+            sensorLongitude2.append(row["longitude"])  
+    
 except FileNotFoundError:
     print("File not found")
 except json.JSONDecodeError:
     print("Invalid JSON")
+
+# Find collisions, rounded to the nearest int
