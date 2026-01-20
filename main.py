84596dad4b9f852be36a5fc8a45bfd431144f5d6
Luis Hernandez <luis.hernandez1@ucalgary.ca>
2026-01-20 02:14:17 -0700

    Collisions algorithm

--- main.py
+++ main.py
@@ -32,4 +32,24 @@ except FileNotFoundError:
 except json.JSONDecodeError:
     print("Invalid JSON")
 
-# Find collisions, rounded to the nearest int
+# Find collisions, rounded to 3 digits
+
+sensor1Coords, sensor2Coords = [], []
+for lat, lon in zip(sensorLatitude1, sensorLongitude1):
+    coord = (round(float(lat), 3), round(float(lon), 3))
+    sensor1Coords.append(coord)
+
+for lat, lon in zip(sensorLatitude2, sensorLongitude2):
+    coord = (round(float(lat), 3), round(float(lon), 3))
+    sensor2Coords.append(coord)
+
+# Turing the cords into sets then finding intersection to find collisions 
+sensor1Set, sensor2Set = set(sensor1Coords), set(sensor2Coords)
+collisions = sensor1Set.intersection(sensor2Set)
+
+if collisions:
+    print(f"Found {len(collisions)} collisions:")
+    for lat, lon in collisions:
+        print(f"Latitude: {lat}, Longitude: {lon}")
+else:
+    print("No collisions found.")
